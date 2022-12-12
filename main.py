c6353bca2ca0e2f2022cfffa3e3fda719ef99d8b
danielcrd <55002280+danielcrd@users.noreply.github.com>
2022-12-12 13:25:35 -0700

    Cleaned up and commented code

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pieces.rook import Rook
 from pieces.bishop import Bishop
 from pieces.knight import Knight
 from pieces.queen import Queen
-from board_projection2 import project_board
-from board_projection2 import extract_digit
+from board_projection import project_board
+from board_projection import extract_digit
 from chessBoard import ChessBoard
 
 
@@ -73,13 +73,6 @@ boardDict = {wp1.position: wp1, wp2.position: wp2, wp3.position: wp3, wp4.positi
 LETTER_DICT: dict = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z'}
 
 
-#boardDict = {wr1.position: wr1, br2.position: br2}
-
-#boardDict = {wb1.position: wb1, bb2.position: bb2}
-
-#boardDict = {wkn1.position: wkn1, bkn1.position: bkn1}
-
-#boardDict = {wk.position: wk, bk.position: bk}
 
 board = ChessBoard(boardDict)
 
@@ -87,22 +80,11 @@ backgroundLocation = "chessBoardBlank.png"
 boardDisplayer = BoardDisplay(backgroundLocation)
 boardDisplayer.update(board.getBoardDict())
 
-#graphicsBoard = Board(750, 750)
-
 playing = True
 turn = "white"
 
 
 markerDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
-
-# marker1 = cv2.aruco.drawMarker(markerDict, 1, sidePixels=750)
-# cv2.imshow("Marker 1", marker1)
-#
-# marker2 = cv2.aruco.drawMarker(markerDict, 2, sidePixels=750)
-# cv2.imshow("Marker 2", marker2)
-#
-# cv2.imwrite("Marker1.png", marker1)
-# cv2.imwrite("Marker2.png", marker2)
 
 
 # Set up video capture from a USB camera
@@ -114,19 +96,16 @@ if not got_img:
 
 board_img = boardDisplayer.getDisplayImg()
 
+
 while playing:
+
+    # Read from the webcam
     got_img, bgr_img = video_capture.read()
     # Project the board onto the current image and show it
-    projected_img, h, _ = project_board(bgr_img, board_img)
+    projected_img, h, _ = project_board(bgr_img, cv2.flip(board_img, flipCode=1))
     cv2.imshow("Chess", projected_img)
     cv2.imshow("Board in 2D", board_img)
     cv2.imshow("Camera Feed", bgr_img)
-
-    # print("Evaluation: ")
-    # print(graphicsBoard.evaluate(boardDict))
-    # boardDict = graphicsBoard.nextMove(boardDict, turn)
-    # boardDisplayer.update(boardDict)
-    # boardDisplayer.display()
 
     # Load letter and digit recognition models
     digit_model = tf.keras.models.load_model("digit_model.h5")
@@ -136,44 +115,47 @@ while playing:
     key_pressed = cv2.waitKey(1) & 0xFF
     if key_pressed == 27 or key_pressed == ord('q'):
         break
+
+    # If the move key (m) was pressed, make a move
     elif key_pressed == ord('m'):
         startLocation = ['Z', -1]
         endLocation = ['X', -2]
-        # for i in range(4):
-        #     digit, success = extract_digit(bgr_img, i)
-        #     if not success:
-        #         print('Could not detect an ArUco marker, please try again')
-        #         continue
-        #     if i == 0:
-        #         startLocation[0] = LETTER_DICT[np.argmax(letter_model.predict(digit.reshape(-1, 28, 28, 1)))]
-        #     elif i == 1:
-        #         startLocation[1] = np.argmax(digit_model.predict(digit.reshape(-1, 28, 28, 1)))
-        #     elif i == 2:
-        #         endLocation[0] = LETTER_DICT[np.argmax(letter_model.predict(digit.reshape(-1, 28, 28, 1)))]
-        #     elif i == 3:
-        #         endLocation[1] = np.argmax(digit_model.predict(digit.reshape(-1, 28, 28, 1)))
-        #
-        #
-        #     if (i % 2):
-        #         print(np.argmax(digit_model.predict(digit.reshape(-1, 28, 28, 1))))
-        #     else:
-        #         print(LETTER_DICT[np.argmax(letter_model.predict(digit.reshape(-1, 28, 28, 1)))])
-        # print(startLocation)
-        # print(endLocation)
+        for i in range(4):
+            # Get the reformatted image of the current letter/digit
+            digit, success = extract_digit(bgr_img, i)
+            if not success:
+                # If there was no ArUco marker, just continue
+                print('Could not detect an ArUco marker, please try again')
+                continue
+            if i == 0:
+                # If this is the first letter, store the letter model's prediction in startLocation[0]
+                startLocation[0] = LETTER_DICT[np.argmax(letter_model.predict(digit.reshape(-1, 28, 28, 1))[0, 1:9]) + 1]
+            elif i == 1:
+                # If this is the first digit, store the digit model's prediction in startLocation[1]
+                startLocation[1] = np.argmax(digit_model.predict(digit.reshape(-1, 28, 28, 1))[0, 1:9]) + 1
+            elif i == 2:
+                # If this is the second letter, store the letter model's prediction in endLocation[0]
+                endLocation[0] = LETTER_DICT[np.argmax(letter_model.predict(digit.reshape(-1, 28, 28, 1))[0, 1:9]) + 1]
+            elif i == 3:
+                # If this is the second digit, store the digit model's prediction in endLocation[0]
+                endLocation[1] = np.argmax(digit_model.predict(digit.reshape(-1, 28, 28, 1))[0, 1:9]) + 1
 
+        # Print the predictions to console for debugging purposes
+        print(startLocation)
+        print(endLocation)
+
+        # Attempt to make the move
         updatedBoard = board.movePiece(startLocation, endLocation, turn)
 
-        #Code to use until cov net gets better
-        if updatedBoard == False:
-            updatedBoard = board.movePiece(['E', 2], ['E', 4], turn)
-
-
+        # IF the move was successful...
         if updatedBoard != False:
 
+            # Update the board image and show it
             boardDisplayer.update(board.getBoardDict())
-            print(board.getBoardDict())
             board_img = boardDisplayer.getDisplayImg()
             cv2.imshow("Updated 2D board", board_img)
+
+            # Switch whose turn it is
             if turn == "white":
               turn = "black"
             elif turn == "black":
